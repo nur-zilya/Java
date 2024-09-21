2357b474f82b2c85824fa743c34f1fb721ea0029
zilya <nur-zilya@mail.ru>
2024-09-21 17:00:03 +0300

    how to jar

--- DateTime/src/Main.java
+++ DateTime/src/Main.java
@@ -1,11 +1,9 @@
 import java.time.LocalTime;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
         System.out.println("Date and time:");
         System.out.println(LocalTime.now());
     }
